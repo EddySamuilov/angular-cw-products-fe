8e317557163126d509554b0132c3605d509d2876
EddySamuilov <samuilow10e@gmail.com>
2024-11-30 22:29:49 +0200

    Update home page product view

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../types/product';
+import { ProductService } from '../product/product.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,19 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  products: Product[] = [];
+  isLoading = true;
 
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe((products) => {
+      console.log(products);
+      
+      this.isLoading = false;
+      this.products = products;
+    })
+  }
+  
 }
