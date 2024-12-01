d8d74b63a4f48cbd11882ee57f939bd0a3b42eca
EddySamuilov <samuilow10e@gmail.com>
2024-12-01 11:56:24 +0200

    Fix shadows. Add category css

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,27 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../types/product';
 import { ProductService } from '../product/product.service';
+import { Category } from '../types/category';
+import { CategoryService } from '../category/category.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent implements OnInit {
   products: Product[] = [];
+  categories: Category[] = [];
   isLoading = true;
 
-  constructor(private productService: ProductService) { }
+  constructor(
+    private productService: ProductService,
+    private categortService: CategoryService
+  ) {}
 
   ngOnInit(): void {
     this.productService.getProducts().subscribe((products) => {
       console.log(products);
-      
+
       this.isLoading = false;
       this.products = products;
+    });
+
+    this.categortService.getCategories().subscribe((categories) => {
+      this.isLoading = false;
+      this.categories = categories;
     })
   }
-  
 }
--- src/app/types/category.ts
+++ src/app/types/category.ts
@@ -1,4 +1,5 @@
 export interface Category {
   type: string;
   description: string;
+  imageUrl: string;
 }
