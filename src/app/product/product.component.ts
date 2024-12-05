f3596e3119bbdc2b4d0dd7f984f99f7ac039a81b
EddySamuilov <samuilow10e@gmail.com>
2024-12-05 09:16:35 +0200

    Add loader to the products page

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../types/product';
 import { ProductService } from './product.service';
 import { ElapsedTimePipe } from '../shared/pipes/elapsed-time.pipe';
-import { DatePipe } from '@angular/common';
+import { DatePipe, TitleCasePipe } from '@angular/common';
 import { ActivatedRoute, RouterLink } from '@angular/router';
+import { LoaderComponent } from '../shared/loader/loader.component';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [RouterLink, ElapsedTimePipe, DatePipe],
+  imports: [RouterLink, ElapsedTimePipe, DatePipe, LoaderComponent, TitleCasePipe],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css',
 })
@@ -27,13 +28,14 @@ export class ProductComponent implements OnInit {
       if (categoryId) {
         this.productService.getProducts(categoryId).subscribe((products) => {
           this.products = products;
+          this.isLoading = false;
         });
       } else {
         this.productService.getProducts(null).subscribe((products) => {
           this.products = products;
+          this.isLoading = false;
         });
       }
-      this.isLoading = false;
     });
   }
 }
